44b849484852b364bdcd078446f2c0229c0dc6fd
Hugo Meireles <meireles.hmm@gmail.com>
2019-01-05 05:13:58 +0000

    moved files to frontend

--- person.js
+++
@@ -1,9 +0,0 @@
-class Person {
-    constructor(name, iliquid, status, nChildren, socialTax){
-        this.name = name;
-        this.iliquid = iliquid;
-        this.status = status;
-        this.nChildren = nChildren;
-        this.socialTax = socialTax;
-    }
-}
